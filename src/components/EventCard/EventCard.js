9f7e7ae1d492a593e6290e6cfaf9a2af071f2950
Mrudul Patel <mrudulpatel04@gmail.com>
2023-02-23 16:48:33 +0530

    routing of events to event details

--- src/components/EventCard/EventCard.js
+++ src/components/EventCard/EventCard.js
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 import classes from "./EventCard.module.css";
 
 const EventCard = (props) => {
-  return window.location.pathname === "/home" ? (
+  return (
     <div className={classes.card}>
       <Link
-        to={window.location.pathname === "/home" ? props.redirectLink : null}
+        to={props.redirectLink}
         style={{ textDecoration: "none", color: "white" }}
       >
         <div className={classes.imgBox}>
@@ -51,51 +51,6 @@ const EventCard = (props) => {
           </>
         ) : null}
       </Link>
-    </div>
-  ) : (
-    <div className={classes.card}>
-      <div className={classes.imgBox}>
-        <img
-          className={classes.img}
-          src={props.imgSrc}
-          alt="poster coming soon"
-        />
-      </div>
-      <div
-        className={
-          window.location.pathname === "/home"
-            ? classes.textBox1
-            : classes.textBox
-        }
-      >
-        <h3 className={classes.heading}>{props.heading}</h3>
-        <Link
-          hidden={window.location.pathname === "/home" ? true : false}
-          className={classes.btn}
-          to={props.redirectLink}
-          onClick={() => {
-            sessionStorage.setItem("eventName", props.heading);
-            sessionStorage.setItem("amount", props.amount);
-            sessionStorage.setItem("qr", props.qr);
-            console.log("Saved");
-          }}
-        >
-          Register Now
-        </Link>
-      </div>
-      {window.location.pathname === "/home" ? (
-        <>
-          <br />
-          <h2>Faculty: {props.faculty}</h2>
-          <br />
-          <h2>Student Leads:</h2>
-          {props.students.map((student, i) => (
-            <h2>
-              {i + 1}. {student.name} - {student.mobile}
-            </h2>
-          ))}
-        </>
-      ) : null}
     </div>
   );
 };
